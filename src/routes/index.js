335a2029411763bc87f62e404f0173a0b5e4caf6
Ananda Farhan <nandafarhan@gmail.com>
2021-08-29 23:36:27 +0700

    User API

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,31 +11,6 @@ const {
 	deleteUser,
 } = require('../controllers/user');
 
-const {
-	getProducts,
-	getProduct,
-	addProduct,
-	updateProduct,
-	deleteProduct,
-} = require('../controllers/product');
-
-const {
-	getToppings,
-	getTopping,
-	addTopping,
-	updateTopping,
-	deleteTopping,
-} = require('../controllers/topping');
-
-const {
-	addTransaction,
-	getTransactions,
-	getTransaction,
-	deleteTransaction,
-	updateTransaction,
-	getUserTransactions,
-} = require('../controllers/transaction');
-
 //* --------------------------  AUTH  ---------------------------- *//
 router.post('/auth/register', register);
 router.post('/auth/login', login);
@@ -45,27 +20,5 @@ router.get('/users', authAdmin, getUsers);
 router.get('/user/:id', authAdmin, getUser);
 router.patch('/user/:id', auth, uploadFile('avatar'), updateUser);
 router.delete('/user/:id', authAdmin, deleteUser);
-
-//* ----------------------  PRODUCT ROUTE  ----------------------- *//
-router.get('/products', getProducts);
-router.get('/product/:id', getProduct);
-router.post('/product', authAdmin, uploadFile('image'), addProduct);
-router.patch('/product/:id', authAdmin, updateProduct);
-router.delete('/product/:id', authAdmin, deleteProduct);
-
-//* ----------------------  TOPPING ROUTE  ----------------------- *//
-router.get('/toppings', getToppings);
-router.get('/topping/:id', getTopping);
-router.post('/topping', authAdmin, uploadFile('image'), addTopping);
-router.patch('/topping/:id', authAdmin, updateTopping);
-router.delete('/topping/:id', authAdmin, deleteTopping);
-
-//* --------------------  TRANSACTION ROUTE  ---------------------- *//
-router.get('/transactions', authAdmin, getTransactions);
-router.get('/transaction/:id', authAdmin, getTransaction);
-router.get('/my-transactions', auth, getUserTransactions);
-router.post('/transaction', auth, addTransaction);
-router.patch('/transaction/:id', authAdmin, updateTransaction);
-router.delete('/transaction/:id', authAdmin, deleteTransaction);
 
 module.exports = router;
