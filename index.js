4354d64fe4e80318f420c4e30a134e1d8657a74f
Ananda Farhan <nandafarhan@gmail.com>
2021-10-11 23:15:14 +0700

    Deploy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const router = require('./src/routes');
 const path = require('path');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
