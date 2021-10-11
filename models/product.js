5406132a2a1b2dcdc27769c5700246b0ad139eef
Ananda Farhan <nandafarhan@gmail.com>
2021-10-11 17:54:46 +0700

    Add status field

--- models/product.js
+++ models/product.js
@@ -17,6 +17,7 @@ module.exports = (sequelize, DataTypes) => {
 			name: DataTypes.STRING,
 			price: DataTypes.INTEGER,
 			image: DataTypes.STRING,
+			status: DataTypes.BOOLEAN,
 		},
 		{
 			sequelize,
